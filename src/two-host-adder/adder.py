6ac5f959ee0b9173fc5fc87563f91d8b28c118e0
littled3092 <drevild01801@gmail.com>
2024-03-19 19:57:17 +0800

    finish udp todo 1

--- src/two-host-adder/adder.py
+++ src/two-host-adder/adder.py
@@ -3,14 +3,14 @@
 import re
 
 from scapy.all import (
-    Ether,
     IntField,
     Packet,
     StrFixedLenField,
     XByteField,
-    bind_layers,
-    srp1,
-    sendp,
+    Ether,
+    IP,
+    UDP,
+    send,
     sniff
 )
 
@@ -25,58 +25,65 @@ class Adder(Packet):
                     XByteField("is_result", 0x00),
                     IntField("num", 0x00) ]
 
-bind_layers(Ether, Adder, type=0x1234)
+class AdderSender:
+    def __init__(self, dest_ip, dest_port = 1234, src_port = 1234, dest_mac = '08:00:00:00:01:03'):
+        self.src_port = src_port
+        self.dest_mac = dest_mac
+        self.dest_ip = dest_ip
+        self.dest_port = dest_port
+        self.seq_num = 0
+    def send(self, num, seq_num = -1):
+        if seq_num == -1:
+            seq_num = self.seq_num
+            self.seq_num += 1
 
-class NumParseError(Exception):
-    pass
+        pkt = (
+            IP(dst=self.dest_ip) / 
+            UDP(sport = self.src_port, dport=self.dest_port) / 
+            Adder(
+                A='A', D='D', ver_maj=0x00, ver_min=0x01, 
+                seq_num=seq_num, is_result=0x00, num=num
+            )
+        )
 
-class OpParseError(Exception):
-    pass
+        send(pkt)
 
-class Token:
-    def __init__(self,type,value = None):
-        self.type = type
-        self.value = value
+class AdderReceiver:
+    def __init__(self):
+        self.filter = 'udp port 1234'
 
-def receiver(pkt):
-    print("number:", pkt[Adder].num, "seq_num:", pkt[Adder].seq_num)
+    def handle_pkt(self, pkt):
+        if pkt.haslayer(Adder):
+            print("Received packet:")
+            print("    num: ", pkt[Adder].num)
+            print("    seq_num: ", pkt[Adder].seq_num)
+
+
+    def receive(self):
+        sniff(filter=self.filter, prn=self.handle_pkt)
 
 def main():
     iface = 'eth0'
 
     node_type = input("Is this a sender or receiver? (s/r): ")
     if node_type == 's':
+        sender = AdderSender(
+            dest_ip = '10.0.1.3',
+            dest_port = 1234
+        )
         while True:
-            num = int(input("Enter a number: "))
-            seq = int(input("Enter a sequence number(0~255): "))
-
-            # send and receive ack
+            num = input("Enter a number to send: ")
             try:
-                pkt = Ether(dst='00:04:00:00:00:00', type=0x1234) / Adder(num=num, seq_num=seq)
-                pkt = pkt/' '
-                resp = srp1(pkt, iface=iface, timeout=1, verbose=False)
-                if resp:
-                    if resp[Adder] and resp[Adder].is_result == 0x00:
-                        print("ACK received, number saved. seq_num:", resp[Adder].seq_num)
-                    elif resp[Adder] and resp[Adder].is_result == 0x01:
-                        print("ACK received, result added. seq_num:", resp[Adder].seq_num, "result:", resp[Adder].num)
-                        resultPkt = Ether(dst='00:04:00:00:00:00', type=0x1234) / Adder(num=resp[Adder].num, seq_num=resp[Adder].seq_num, is_result=0x01)
-                        resultPkt = resultPkt/' '
-                        sendp(resultPkt, iface=iface, verbose=False)
-                    else:
-                        print("Cannot parse ACK")
-                else:
-                    print("ACK not received")
-            except Exception as error:
-                print(error)
+                num = int(num)
+            except ValueError:
+                print("Invalid number")
+                continue
+            sender.send(num)
 
     elif node_type == 'r':
+        receiver = AdderReceiver()
         while True:
-            try:
-                sniff(iface=iface, prn=receiver, filter="ether proto 0x1234", count=1, timeout = 1)
-            except Exception as error:
-                print(error)
-
+            receiver.receive()
 
 if __name__ == '__main__':
     main()
