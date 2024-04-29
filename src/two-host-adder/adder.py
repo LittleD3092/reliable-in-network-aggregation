7242b2051bbb3597f176755dc3bad41ba0587b90
Zhih25 <cliffwjz0351@gmail.com>
2024-04-30 00:54:52 +0800

    edit todo

--- src/two-host-adder/adder.py
+++ src/two-host-adder/adder.py
@@ -83,7 +83,7 @@ class AdderSender:
             self.socket.send(payload)
             self.tui.print("[SEND] seq_num: " + str(seq_num) + " num: " + str(num))
             seq_num += 1
-            time.sleep(0.5)
+            time.sleep(0.01)
 
     def run_thread(self):
         t = threading.Thread(target=self.listen_for_ack)
